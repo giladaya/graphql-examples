e8c27e8f2dbca7d8c29c3e6301c4cae7e2d46ac1
giladd <giladd@trainologic.com>
2019-07-20 23:32:50 +0300

    add friends and create

--- apollo/src/sampleData.js
+++ apollo/src/sampleData.js
@@ -6,32 +6,38 @@ const users = [
   {
     id: "aaa",
     name: "Alice",
-    age: 16
+    age: 16,
+    friends: ["bbb", "ccc"]
   },
   {
     id: "bbb",
     name: "Bob",
-    age: 17
+    age: 17,
+    friends: ["aaa"]
   },
   {
     id: "ccc",
     name: "Charlie",
-    age: 18
+    age: 18,
+    friends: ["aaa", "ddd"]
   },
   {
     id: "ddd",
     name: "Dave",
-    age: 19
+    age: 19,
+    friends: ["ccc"]
   },
   {
     id: "eee",
     name: "Erin",
-    age: 20
+    age: 20,
+    friends: []
   },
   {
     id: "fff",
     name: "Frank",
-    age: 21
+    age: 21,
+    friends: []
   }
 ];
 
@@ -41,7 +47,18 @@ function fetchUsers() {
 function fetchUserById(id) {
   return users.find(user => user.id === id);
 }
+function createUser(name, age) {
+  const newId = Date.now();
+  const newUser = {
+    id: newId.toString(),
+    name,
+    age
+  };
+  users.push(newUser);
+  return newUser;
+}
 module.exports = {
   fetchUsers,
-  fetchUserById
+  fetchUserById,
+  createUser
 };
